bf8a46bd69e439b44e794773e0512a26c526c3aa
Mishra-Atharva <atharvamishramain@gmail.com>
2022-08-25 23:49:50 +0800

    new fix currency converter api

--- currency-converter-api/script.js
+++ currency-converter-api/script.js
@@ -1,8 +1,27 @@
 console.log("testing...");
 
+// Initalizing variables
+var currentcurr;
+var currentlist;
+var newlink
+var keyword;
+var reversekey;
+var reverselink;
+var oldcurr;
+var oldlist;
+var oldlink;
+var from = document.getElementById("list");
+var to = document.getElementById("list2");
+from.addEventListener("click", () => {
+  getrate();
+});
+to.addEventListener("click", () => {
+  getrate();
+})
+
 // Authentication
 var myHeaders = new Headers();
-    myHeaders.append("apikey", "DFdORjPe0DcRPe1d5Gf52ZmwisKVmrOA");
+    myHeaders.append("apikey", "V5CEPdqCYlWSBoJVqn5W8rvNFDMFxB2E");
     var requestOptions = {
         method: 'GET',
         redirect: 'follow',
@@ -32,30 +51,20 @@ window.addEventListener("load", () => {
         option2.text = Object.keys(response.currencies)[i];
         option2.value = Object.keys(response.currencies)[i];
         list.append(option);
-        list2.append(option2);  
+        list2.append(option2);
       }
     })
-  });
-
-var currentcurr;
-var currentlist;
-var newlink
-var reverselink;
-var oldcurr;
-var oldlist;
-var oldlink;
-
-var from = document.getElementById("list");
-var to = document.getElementById("list2");
-from.addEventListener("click", () => {
-  getrate();
 });
+getrate();
 
 function getrate(){
   oldcurr = currentcurr;
   oldlist = currentlist;
   newlink = "https://api.apilayer.com/currency_data/live?source="+from.value+"&currencies="+currencies;
-  if (newlink == oldlink || newlink == reverselink) {
+  console.log(newlink);
+  console.log(from.value);
+  console.log(to.value);
+  if (newlink == oldlink || newlink == reverselink && (from.value + to.value) != keyword) {
     console.log("This link has been used before therefore no request to API was sent");
   } else {
     console.log("This link hasn't been used before therefore request to API was sent");
@@ -72,18 +81,29 @@ const message = document.querySelector("#user");
 message.addEventListener('input', function () {
   oldlink = "https://api.apilayer.com/currency_data/live?source="+from.value+"&currencies="+currencies;
   reverslink = "https://api.apilayer.com/currency_data/live?source="+to.value+"&currencies="+currencies;
-  var keyword = from.value + to.value;
-  var reversekey = to.value + from.value;
-  if (Object.keys(oldlist).includes(reversekey)) {
-    var exchangerate = oldlist[reversekey];
-    var amount = message.value / exchangerate;
-    document.getElementById("value2").value = "$ " + amount.toFixed(2);
-  } else if (Object.keys(oldlist).includes  (keyword)) {
+  keyword = from.value + to.value;
+  reversekey = to.value + from.value;
+  if (from.value == to.value) {
+    currentcurr = from.value;
+    document.getElementById("value2").value = "$ " + message.value;
+  } else if (Object.keys(oldlist).includes(keyword) && true) {
     var exchangerate = oldlist[keyword];
     var amount = message.value * exchangerate;
     document.getElementById("value2").value = "$ " + amount.toFixed(2);
+  } else if (Object.keys(oldlist).includes(reversekey) && true) {
+    var exchangerate = oldlist[reversekey];
+    var amount = message.value / exchangerate;
+    document.getElementById("value2").value = "$ " + amount.toFixed(2);
+  } else if (Object.keys(oldlist).includes(reversekey) || Object.keys(oldlist).includes(keyword) || from.value == from.value) {
+    fetch("https://api.apilayer.com/currency_data/live?source="+from.value+"&currencies="+currencies, requestOptions)
+      .then((a) => a.json())
+      .then((response) => {
+        currentlist = response.quotes;
+        var exchangerate = currentlist[keyword];
+        var amount = message.value * exchangerate;
+        document.getElementById("value2").value = "$ " + amount.toFixed(2);
+      })
   } else {
-    currentcurr = from.value;
     var exchangerate = currentlist[keyword];
     var amount = message.value * exchangerate;
     document.getElementById("value2").value = "$ " + amount.toFixed(2);
